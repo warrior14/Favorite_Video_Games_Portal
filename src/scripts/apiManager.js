c4caaa5479853be0c5e1c15b56f91b9743733886
warrior14 <ironaddicts14@gmail.com>
2021-03-15 19:18:58 -0500

    made changes to JSON

--- src/scripts/apiManager.js
+++ src/scripts/apiManager.js
@@ -25,10 +25,10 @@ export default class ApiManager {
     };
     addVideoGames(newVideoGame) {
         try {
-            return fetch("http://localhost:8088/videoGames", {
+            return fetch("http://localhost:8088/videoGames/", {
                 method: "POST",
                 headers: {
-                    "Content-Type": "application/json"
+                    "Content-Type": "application/json" 
                 },
                 body: JSON.stringify(newVideoGame)
             }).then(response => response.json());
@@ -36,6 +36,54 @@ export default class ApiManager {
             console.error(e);
         };
     };
-};
+
+    deleteVideoGames(videoGameId) {
+        try {
+            return fetch(`http://localhost:8088/videGames/${videoGameId}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            }).then(response => response.json());
+        } catch (e) {
+            console.error(e);
+        };
+    };
 
 
+
+    editVideoGame(videoGameId, data) {
+    try {
+        return fetch(`http://localhost:8088/videoGames/${videoGameId}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+            body: JSON.stringify(data)
+        });
+    
+    } catch (e) {
+        console.error(e);
+        };
+    };
+
+
+    
+    patchVideoGame(videoGameId, newVideoGame) {
+        try {
+            return fetch(`http://localhost:8088/videoGames/${videoGameId}`,  {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+            },
+                body: JSON.stringify(newVideoGame)
+        });
+        } catch (e) {
+            console.error(e);
+        };
+    };
+
+
+
+
+}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,17 +18,23 @@
 import ApiManager from "./apiManager.js";
 import ElementFactory from "./elementFactory.js";
 
-let parentElementName = document.getElementById('play');
 let apiManager = new ApiManager();
 let elementFactory = new ElementFactory();
 let videoGamesDataPromiseArray = new Array();
 let usersDataPromiseArray = new Array();
 let genresDataPromiseArray = new Array();
 
-let newVideoGame = {};
+let parentElementName = document.getElementById('play');
 let addVideoGameButton = document.getElementById('addVideoGame');
 let inputVideoGameName = document.getElementById('name');
 let inputVideoGameGenre = document.getElementById('genre');
+let inputVideoGameId = document.getElementById('videoGameIdInput');
+let deleteVideoGameButton = document.getElementById('deleteVideoGame');
+let editVideoGameButton = document.getElementById('editVideoGame');
+let patchVideoGameButton = document.getElementById('patchVideoGame');
+
+let newVideoGame = {};
+let videoGameId;
 
 let getDataFromApis = () => {
     videoGamesDataPromiseArray.push(apiManager.getVideoGames());
@@ -40,16 +46,42 @@ let addEventListeners = () => {
     inputVideoGameName.addEventListener('change', updateValue);
     inputVideoGameGenre.addEventListener('change', updateValue);
     addVideoGameButton.addEventListener('click', clickOnAddVideoGame);
+    inputVideoGameId.addEventListener('change', updateValue);
+    deleteVideoGameButton.addEventListener('click', clickOnDeleteVideoGame);
+    editVideoGameButton.addEventListener('click', clickOnEditVideoGame);
+    patchVideoGameButton.addEventListener('click', clickOnPatchVideoGame);
 };
 
 let updateValue = (event) => {
-    newVideoGame[event.target.id] = event.target.value;
-    console.log('videoGame object', newVideoGame);
+    if(event.target.id === 'videoGameIdInput') {
+        videoGameId = event.target.value;
+    } else {
+        newVideoGame[event.target.id] = event.target.value; 
+        console.log('videoGame object', newVideoGame);
+    };
 };
-
 let clickOnAddVideoGame = () => {
     apiManager.addVideoGames(newVideoGame).then(() => {
         location.reload();
+    });
+};
+
+
+let clickOnEditVideoGame = () => {
+    apiManager.editVideoGame(videoGameId, newVideoGame).then(() => {
+        location.reload();
+    });
+};
+
+let clickOnDeleteVideoGame = () => {
+    apiManager.deleteVideoGames(videoGameId).then(() => {  
+        location.reload();      
+    });
+};
+
+let clickOnPatchVideoGame = () => {
+    apiManager.patchVideoGame(videoGameId, newVideoGame).then(() => {
+       location.reload(); 
     });
 };
 
